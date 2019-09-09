c4a9b8a14b97d4aa67cf6eeb283fed5a48f54da3
rafael.escobar <rafael.escobar@MTD-W10-Escobar.microton.com.br>
2019-09-09 14:12:50 -0300

    qua

--- src/Principal.java
+++ src/Principal.java
@@ -3,7 +3,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("foi mesmo outra");
+		System.out.println("foi mesmo outra na b");
 	}
 
 }
